98c173e7d609065ac1ce34f303981f4be4c805c9
Jaliya Madusanka <87718010+Jaliya98@users.noreply.github.com>
2024-03-10 12:19:40 +0530

    Added application properties

--- ebookstore/src/main/java/com/nsbm/ebookstore/donation/model/donationModel.java
+++ ebookstore/src/main/java/com/nsbm/ebookstore/donation/model/donationModel.java
@@ -1,2 +1,19 @@
-package com.nsbm.ebookstore.donation.model;public class donationModel {
+package com.nsbm.ebookstore.donation.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class donationModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String isbn;
+    private String bookName;
+    private String author;
+    private String donationType;
 }
