b601c22981a189330154651757a54eff895882aa
Tharuksha <tharukshawickramarachchi@gmail.com>
2024-03-22 00:46:47 +0530

    CORS Configuration

--- ebookstore/src/main/java/com/nsbm/ebookstore/login/Controller/UserController.java
+++ ebookstore/src/main/java/com/nsbm/ebookstore/login/Controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 @RequestMapping("/api")
 public class UserController {
     @Autowired
