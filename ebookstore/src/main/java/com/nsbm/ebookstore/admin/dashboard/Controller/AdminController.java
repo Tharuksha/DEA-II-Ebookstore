c56804535d9676ee344c271f82e2ac662bab2abe
Tharuksha <tharukshawickramarachchi@gmail.com>
2024-03-28 17:56:09 +0530

    dashboard CRUD Fix

---
+++ ebookstore/src/main/java/com/nsbm/ebookstore/admin/dashboard/Controller/AdminController.java
@@ -0,0 +1,63 @@
+package com.nsbm.ebookstore.admin.dashboard.Controller;
+
+import com.nsbm.ebookstore.admin.dashboard.Entity.Users;
+import com.nsbm.ebookstore.admin.dashboard.Service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST})
+@RequestMapping("/api/users")
+public class AdminController {
+
+    @Autowired
+    private AdminService adminService;
+
+    @GetMapping("/admins")
+    public ResponseEntity<List<Users>> getAdminUsers() {
+        List<Users> adminUsers = adminService.getAdminUsers();
+        return new ResponseEntity<>(adminUsers, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Users> getUserById(@PathVariable("id") Long id) {
+        Users user = adminService.getUserById(id);
+        if (user != null) {
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Users> createUser(@RequestBody Users user) {
+        Users savedUser = adminService.saveUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Users> updateUser(@PathVariable("id") Long id, @RequestBody Users user) {
+        Users existingUser = adminService.getUserById(id);
+        if (existingUser != null) {
+            user.setId(id.intValue()); // Convert Long to Integer
+            Users updatedUser = adminService.saveUser(user);
+            return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable("id") Long id) {
+        boolean deleted = adminService.deleteUser(id);
+        if (deleted) {
+            return new ResponseEntity<>("User with ID " + id + " deleted successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("User with ID " + id + " not found", HttpStatus.NOT_FOUND);
+        }
+    }
+}
