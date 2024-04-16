e78e639d5c20749d9dbdeae40b36282c93af9555
Neranjan01 <mrnbekanayake@students.nsbm.ac.lk>
2024-04-16 13:15:11 +0530

    Changed book_model

--- ebookstore/src/main/java/com/nsbm/ebookstore/book_service/model/BookModel.java
+++ ebookstore/src/main/java/com/nsbm/ebookstore/book_service/model/BookModel.java
@@ -13,11 +13,12 @@ import java.sql.Date;
 public class BookModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long bookId;
     private String isbn;
     private String bookName;
     private String bookAuthor;
     private String publisher;
     private Date addedOn;
     private int quantity;
+    private String imgName;
 }
