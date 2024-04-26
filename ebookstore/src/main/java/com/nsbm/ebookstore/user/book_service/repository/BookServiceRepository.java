e7e77e550017fa775b31a04fd531c8f5b2f83452
Tharuksha <tharukshawickramarachchi@gmail.com>
2024-04-27 02:22:35 +0530

    merge

--- ebookstore/src/main/java/com/nsbm/ebookstore/user/book_service/repository/BookServiceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nsbm.ebookstore.book_service.repository;
-
-import com.nsbm.ebookstore.book_service.model.BookModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookServiceRepository extends JpaRepository<BookModel, Long> {
-}
